d4835799b0fdb414cdd65ab123aee4d9b22f4f45
Kai Du <alanduu50@gmail.com>
2016-11-21 19:14:04 +0000

    Minor refactoring

--- generate_reply_completed.py
+++ generate_reply_completed.py
@@ -5,6 +5,22 @@ import random
 greetings = [ "hi", "hello", "hey", "yo", "greetings" ]
 greetings_responses = [ "Hi there." , "Greetings human.", "Hello there.", "Hey." ]
 
+# Returns JJ if sentence structure is You Are {word}+ JJ {word}+.
+def findYouAreJJ(pos):
+    foundYou = False
+    foundYouAre = False
+
+    for e in pos:
+        if e[0].lower() == 'you':
+            foundYou = True
+        elif e[0].lower() == 'are' and foundYou:
+            foundYouAre = True
+        elif foundYou and not foundYouAre:
+            foundYou = False
+        elif foundYouAre and e[1] == 'JJ':
+            return e[0]
+    return False
+
 # Generates a bot response from a user message
 def generateReply(message):
     pos = getPOS(message)
@@ -15,7 +31,7 @@ def generateReply(message):
         return "I am not functioning at the moment. Perhaps check your API keys."
 
     # If user greeted
-    if pos[0][0] in greetings:
+    if pos[0][0].lower() in greetings:
         return random.choice(greetings_responses)
 
     # If user said 'You are ... {adjective} ...'
@@ -40,39 +56,18 @@ def generateReply(message):
         return "I feel sad about that."
 
 
-# Returns JJ if sentence structure is You Are {word}+ JJ {word}+.
-def findYouAreJJ(pos):
-    foundYou = False
-    foundAre = False
-
-    for e in pos:
-        if e[0].lower() == 'you':
-            foundYou = True
-            continue
-        if e[0].lower() == 'are' and foundYou:
-            foundAre = True
-            continue
-        if foundYou and not foundAre:
-            return False
-        if foundAre and e[1] == 'JJ':
-            return e[0]
-    return False
-
-
 # Returns JJ if sentence structure is I Am {word}+ JJ {word}+.
 def findIAmJJ(pos):
     foundI = False
-    foundAm = False
+    foundIAm = False
 
     for e in pos:
         if e[0].lower() == 'i':
             foundI = True
-            continue
-        if e[0].lower() == 'am' and foundI:
-            foundAm = True
-            continue
-        if foundI and not foundAm:
-            return False
-        if foundAm and e[1] == 'JJ':
+        elif e[0].lower() == 'am' and foundI:
+            foundIAm = True
+        elif foundI and not foundIAm:
+            foundI = False
+        elif foundIAm and e[1] == 'JJ':
             return e[0]
     return False
